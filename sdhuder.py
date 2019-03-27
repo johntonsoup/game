22a46091a381bac894762578727330ceee04a384
johntonsoup <42421680+johntonsoup@users.noreply.github.com>
2019-03-27 10:52:17 -0700

    Add files via upload

---
+++ sdhuder.py
@@ -0,0 +1,11 @@
+print("Welcome to Tic-Tac-Toe!")
+board = [[ "_","_" ,"_" ]]
+        
+board2 = [["_", "_", "_" ]]
+       
+board3 = [["_", "_" , "_"]]
+print(board)
+print(board2)
+print(board3)
+      
+
